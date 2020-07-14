279462dbebee0ca73811fc93ab59c38f47d6e78d
NedosekinStas <stasnedosekin@mail.ru>
2020-07-14 11:17:26 +0300

    add page visit

--- app.rb
+++ app.rb
@@ -11,8 +11,11 @@ end
 class Specialist < ActiveRecord::Base
 end
 
-get '/' do
+before do 
   @specialists = Specialist.all
+end
+
+get '/' do
   erb :index
 end
 
@@ -22,4 +25,25 @@ end
 
 get '/about' do
   erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @specialist = params[:specialist]
+  @color = params[:color]
+
+  hh = {
+    :username => 'Введите имя', 
+    :phone => 'Введите телефон', 
+    :datetime => 'Введите дату и время'
+  }
+
+  erb "<h2>Спасибо, мы ждем вас!</h2>"
 end
---
+++ public/vendor/script/common.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('.colorpicker').simplecolorpicker();
+    $('#datetimepicker').datetimepicker();
+});
